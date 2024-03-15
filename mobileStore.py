bbff72f84725bc234098a5770fca3f5362d38ebb
RAKIBUL ISLAM <rakibul8226@gmail.com>
2024-03-15 22:17:23 +0600

    user place order created

--- mobileStore.py
+++ mobileStore.py
@@ -5,9 +5,9 @@ class MobileStoreManagement:
             "user": {"password": "user"}
         }
         self.products = {
-            1: {"name": "Xiaomi", "price": 10, "color": "Red"},
-            2: {"name": "Samsung", "price": 20, "color": "Blue"},
-            3: {"name": "Apple", "price": 30, "color": "Green"},
+            1: {"name": "Apple", "price": 10, "color": "Red"},
+            2: {"name": "Xiaomi", "price": 30, "color": "Green"},
+            3: {"name": "Samsung", "price": 20, "color": "Blue"},
             # Add more products as needed
         }
         self.user_orders = {}
@@ -36,17 +36,21 @@ class MobileStoreManagement:
             else:
                 print("Invalid choice. Please enter a valid option.")
 
-    # ---------------------------------------User----------------------------------------------------
+    # ---------------------------------------User Start----------------------------------------------------
     def user_panel(self):
         print()
         print("**********WELCOME AS AN USER **********")
         while True:
 
-            print("a. Logout")
+            print("\nUser Panel:")
+            print("a. Place Order")
+            print("b. Logout")
             print()
             choice = input("Enter your choice: ")
 
             if choice == 'a':
+                self.place_order()
+            elif choice == 'b':
                 print()
                 print("****YOU ARE SUCCESSFULLY LOGGED OUT****")
                 return
@@ -54,7 +58,31 @@ class MobileStoreManagement:
                 print("Invalid choice. Please enter a valid option.")
 
 
-# -------------------------------------------------------------------------------------------
+    def place_order(self):
+        print("Available Products:")
+        for product_id, product_info in self.products.items():
+            print(f"{product_id}. {product_info['name']} - Price: ${int(product_info['price'])} - Color: {product_info['color']}")
+
+        print()
+        product_choice = input("Enter the product ID to place an order: ")
+        if product_choice.isdigit():
+            product_id = int(product_choice) #must change the datatype to int
+            if product_id in self.products:
+                quantity = int(input("Enter the Quantity :"))
+                if quantity > 0:
+                    if product_id in self.user_orders:
+                        self.user_orders[product_id] += quantity
+                        print("Quantity Updated")
+                    else:
+                        self.user_orders[product_id] = quantity
+                        print(f"Order placed: {quantity} {self.products[product_id]['name']}")
+                else:
+                    print("Invalid quantity. Quantity must be greater than 0.")
+            else:
+                print("Invalid product ID")
+        else: print("Invalid input. Please enter a numeric product ID.")
+
+# -----------------------------------------User End--------------------------------------------------
 
 
 if __name__ == "__main__":
